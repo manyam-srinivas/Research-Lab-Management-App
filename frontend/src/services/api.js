64c50021ef784822e841fb7551a71dc8d89f3f50
manyam-srinivas <mlsrinivas231@gmail.com>
2026-07-30 23:11:11 +0530

    Use Render backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://research-lab-backend-x84w.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
